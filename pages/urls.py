82e85a12bca5a6948a738aa61acbcfe123a96d5e
Ali Mamdouh <45511037+loll96@users.noreply.github.com>
2021-03-06 17:29:51 +0200

    add paginat

--- pages/urls.py
+++ pages/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', 
-        login_required(PostListView.as_view(
-        context_object_name = "posts",
-        template_name = "pages/home.html",
-        )),
-        name="home"),
+    path('', login_required(PostListView.as_view()),name="home"),
+    path('posts/<str:username>/', UserPostListView.as_view(), name="user_posts"),
     path('post/<int:pk>', PostDetailView.as_view(), name="post"),
     path('post/create', PostCreateView.as_view(), name="create"),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name="update"),
